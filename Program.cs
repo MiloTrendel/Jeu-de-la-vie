f6d8d86c44b5c7dc4cf541f12a63ca6add32eee9
Milo Trendel <trendel.milo@gmail.com>
2023-11-03 15:25:01 +0100

    Program.cs V1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using TD_Jeu_de_la_vie;
 
 namespace TD_Jeu_de_la_vie
 {
-    class Program
+    static class Program
     {
-
+        static void Main()
+        {
+            Game game = new Game(3, 10);
+            game.RunGameConsole();
+        }
     }
 }
