d224640126562706162829537758930142b50845
Milo Trendel <trendel.milo@gmail.com>
2023-11-03 11:40:13 +0100

    Cell.cs V1

--- Cell.cs
+++ Cell.cs
@@ -1,9 +1,37 @@
-﻿    using System;
+﻿using System;
 
-    namespace TD_Jeu_de_la_vie.Files
+namespace TD_Jeu_de_la_vie.Files
+{
+    class Cell
     {
-        class Cell
+        private bool _isAlive;
+        public bool IsAlive
         {
-            
+            get { return _isAlive; }
+            set { _isAlive = value; }
+        }
+
+        private bool _nextState;
+        public bool NextState { get { return _nextState; } set { _nextState = value; } }
+
+        public Cell(bool state)
+        {
+            IsAlive = state;
+        }
+
+        public void ComeAlive()
+        {
+            NextState = true;
+        }
+
+        public void PassAway()
+        {
+            NextState = false;
+        }
+
+        public void Update()
+        {
+            IsAlive = NextState;
         }
     }
+}
